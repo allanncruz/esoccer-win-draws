4077ffab476b399e18350cbf3c8f53ce972b684c
allan <allan.dantas@redspark.io>
2023-11-02 18:34:19 -0300

    feat: config styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle<{}>`
+
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  body {
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
