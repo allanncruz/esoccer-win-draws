1b3574a906cbacc1fa45dfa37732f4b00237be4b
Allan <contato@allancruz.com.br>
2023-11-03 22:12:37 -0300

    feat: config axios

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,10 +1,43 @@
 import { useState, useEffect } from "react";
 
-const useAxios = {configObg} => {
+const useAxios = (configObg: any) => {
     const {
-        axiosInterface,
+        axiosInstance,
         method,
         url,
         requestConfig = {}
-    } = configObj;
+    } = configObg;
+
+    const [response, setResponse] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const controller = new AbortController();
+
+      const fetchData = async () => {
+        try {
+          const res = await axiosInstance[method.toLowerCase()](url, {
+            ...requestConfig,
+            signal: controller.signal
+          });
+          console.log(res)
+          setResponse(res.data)
+        } catch (err) {
+            console.log(err)
+            setError(err)
+        } finally {
+          setLoading(false)
+        }
+      }
+
+      fetchData();
+
+      return () => controller.abort();
+
+    },[])
+
+    return [response, error, loading];
 }
+
+export default useAxios;
