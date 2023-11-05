7bc002245fada87e2c4c730428a9ba03af1d912f
allan <allan.dantas@redspark.io>
2023-11-05 09:24:48 -0300

    feat: wip request gols

--- src/pages/GtLeague/index.tsx
+++ src/pages/GtLeague/index.tsx
@@ -1,47 +1,49 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Container } from "../../styles/style";
-import { ResultsBlocks } from "./style";
+import { ResultsBlocks, ResultsGoals } from "./style";
 
-type Data = {
+
+
+type Teams = {
+  participant: {
+    team :{
+      name: string
+    }
+  }
+};
+
+interface Data {
   result: {
     stats: {
       away_score: number,
       home_score: number
     }
-  }
+  },
+  participants: Teams;
   channel: string;
   id: number
 };
 
-
-
-const initialResults = "https://hr.gtleagues.com/api/fixtures?kickoff=between%3A2023-11-03T03%3A00%3A00.000Z%2C2023-11-04T02%3A59%3A59.999Z&limit=500&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true";
+const initialResults = "https://hr.gtleagues.com/api/fixtures?kickoff=between%3A2023-11-04T03%3A00%3A00.000Z%2C2023-11-05T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true";
 
 
 
 export default function GtLeague() {
   const [post, setPost] = useState<Data[]>([]);
-  const [isloading, setIsLoading] = useState(false);
+
 
   async function resultData() {
-    setIsLoading(true);
+
     try {
       axios.get(initialResults).then((response) => {
         setPost(response.data);
       });
-
- 
-      // const homeScore = post?.map(item => item.result.stats.home_score)
-      // const awayScore = post?.map(item => item.result.stats.away_score)
-
-      // console.log("homeScore", homeScore) 
-
-      // console.log("awayScore", awayScore)
-
-    } catch (error) {
-      setIsLoading(false);
-    }
+      
+      
+      console.log("response.data", post)
+      
+    } catch (error) {}
   }
 
   useEffect(() => {
@@ -51,17 +53,21 @@ export default function GtLeague() {
 
   return (
     <Container>
-      {isloading ? (
+      {post.length <= 0 ? (
         <span>loading</span>
       ) : (
         <>
           {post?.map((item) => (
             <ResultsBlocks key={item.id}>
-              <label htmlFor="homeScore">Home score</label>
-              <p id="homeScore">{item.result.stats.home_score}</p>
-
-              <label htmlFor="awayScore">Away dcore</label>
-              <p id="awayScore">{item.result.stats.away_score}</p>
+              <label htmlFor="homeScore">
+              {item.participants?.map((teams: Teams) => (
+                <>{teams.participant.team.name}</>
+              ))}
+              </label>
+              <ResultsGoals id="homeScore" children={item.result.stats.home_score} />
+              X
+              <ResultsGoals id="awayScore" children={item.result.stats.away_score} />
+              <label htmlFor="awayScore">Away score</label>
             </ResultsBlocks>
           )
         )}
--- src/pages/GtLeague/style.ts
+++ src/pages/GtLeague/style.ts
@@ -3,4 +3,20 @@ import styled from 'styled-components'
 export const ResultsBlocks = styled.div`
   padding: 10px;
   margin: 5px 0;
+  display: flex;
+  align-items: center;
+
+  label{
+    text-transform: uppercase;
+    font-size: 12px;
+  }
+`;
+
+
+export const ResultsGoals = styled.span`
+  font-size: 22px;
+  width: 20px;
+  text-align: center;
+  margin: 0 3px;
+  font-weight: 500;
 `;
