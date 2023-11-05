409fa1a84bb35d05ddd3c19d391cb1a6dcd30050
Allan <contato@allancruz.com.br>
2023-11-05 14:17:20 -0300

    feat: set draw and win

--- src/pages/GtLeague/index.tsx
+++ src/pages/GtLeague/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Container } from "../../styles/style";
-import { ResultsBlocks, ResultsGoals, ResultsShield } from "./style";
+import { Blocks, Goals, Shield, Kickoff } from "./style";
+import { format, parseISO } from "date-fns";
 
 
 const initialResults = "https://hr.gtleagues.com/api/fixtures?kickoff=between%3A2023-11-04T03%3A00%3A00.000Z%2C2023-11-05T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true";
@@ -25,6 +26,7 @@ interface Data {
       home_score: number
     }
   },
+  kickoff: any,
   participants: Participants[];
   channel: string;
   id: number
@@ -42,7 +44,7 @@ export default function GtLeague() {
       });
       
       
-      console.log("response.datas", post)
+      console.log("response.data", post)
       
     } catch (error) {}
   }
@@ -50,7 +52,14 @@ export default function GtLeague() {
   useEffect(() => {
     resultData()
 
-  }, []);
+  },[]);
+
+  const formatDateDMA = (date?: any) => {
+    const newDate: any = format(parseISO(date), "yyyy-MM-dd HH:mm");
+    const dateConvert = format(new Date(newDate), "dd/MM/yyyy HH:mm");
+  
+    return dateConvert;
+  }
 
   return (
     <Container>
@@ -59,27 +68,31 @@ export default function GtLeague() {
       ) : (
         <>
           {post?.map((item) => (
-            <ResultsBlocks 
-              key={item.id} 
-              className={(item.result.stats.home_score === item.result.stats.home_score) && 'empate'}>
-              <div>
-                <ResultsShield
-                  src={item.participants[0].participant.team.crest} 
-                  alt={item.participants[0].participant.team.name} />
-                <label htmlFor="homeScore">{item.participants[0].participant.team.name}</label>
-                <b>{item.participants[0].participant.player.nickname}</b>
-              </div>
-              <ResultsGoals id="homeScore" children={item.result.stats.home_score} />
-              X
-              <ResultsGoals id="awayScore" children={item.result.stats.away_score} />
-              <div>
-                <ResultsShield
-                  src={item.participants[1].participant.team.crest} 
-                  alt={item.participants[1].participant.team.name} />
-                <label htmlFor="awayScore">{item.participants[1].participant.team.name}</label>
-                <b>{item.participants[1].participant.player.nickname}</b>
-              </div>
-            </ResultsBlocks>
+            <>
+              <Kickoff>{formatDateDMA(item.kickoff)}</Kickoff>
+              <Blocks 
+                key={item.id} 
+                className={item.result.stats.home_score === item.result.stats.away_score ? 'draw' : 'win'}>
+                
+                <div>
+                  <Shield
+                    src={item.participants[0].participant.team.crest} 
+                    alt={item.participants[0].participant.team.name} />
+                  <label htmlFor="homeScore">{item.participants[0].participant.team.name}</label>
+                  <b>{item.participants[0].participant.player.nickname}</b>
+                </div>
+                <Goals id="homeScore" children={item.result.stats.home_score} />
+                X
+                <Goals id="awayScore" children={item.result.stats.away_score} />
+                <div>
+                  <Shield
+                    src={item.participants[1].participant.team.crest} 
+                    alt={item.participants[1].participant.team.name} />
+                  <label htmlFor="awayScore">{item.participants[1].participant.team.name}</label>
+                  <b>{item.participants[1].participant.player.nickname}</b>
+                </div>
+              </Blocks>
+            </>
           )
         )}
         </>
--- src/pages/GtLeague/style.ts
+++ src/pages/GtLeague/style.ts
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
 
-export const ResultsBlocks = styled.div`
+export const Kickoff = styled.small`
+  display: block;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+
+export const Blocks = styled.div`
   padding: 10px;
-  margin: 5px 0;
+  margin: 5px auto;
   display: flex;
   align-items: center;
+  max-width: 350px;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
 
   label{
     font-size: 11px;
+  }
+
+  &.draw{
+    background-color: red;
+  }
+
+  &.win{
+    background-color: green;
   }
 
   > div {
@@ -18,8 +37,7 @@ export const ResultsBlocks = styled.div`
   }
 `;
 
-
-export const ResultsGoals = styled.span`
+export const Goals = styled.span`
   font-size: 22px;
   width: 20px;
   text-align: center;
@@ -27,9 +45,7 @@ export const ResultsGoals = styled.span`
   font-weight: 500;
 `;
 
-
-
-export const ResultsShield = styled.img`
+export const Shield = styled.img`
   width: 38px;
   height: 38px;
   object-fit: contain;
